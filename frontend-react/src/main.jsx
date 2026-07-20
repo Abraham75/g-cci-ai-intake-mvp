8dc5119307e9223f1e408fe693ef53fb04c0390a
Abraham75 <44035311+Abraham75@users.noreply.github.com>
2026-07-20 14:42:19 -0400

    Switch React entrypoint to live FastAPI-backed G-CCI app

--- frontend-react/src/main.jsx
+++ frontend-react/src/main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import GCCIApp from "./GCCIApp.jsx";
+import GCCIAppLive from "./GCCIAppLive.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <GCCIApp />
+    <GCCIAppLive />
   </React.StrictMode>,
 );
